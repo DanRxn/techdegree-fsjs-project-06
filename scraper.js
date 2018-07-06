ea4a89ee6d56bc435a659c8f2b4f5c25f92c6875
Dan Robertson <dan@productintuition.com>
2018-07-06 10:33:29 -0700

    Create the  folder, unless it already exists

--- scraper.js
+++ scraper.js
@@ -1,1 +1,18 @@
-console.log("scraper.js was run!");
+// Require modules
+const fs = require('fs'); // For file system interactions
+const Crawler = require('crawler');
+
+// Create `data` folder, if not already existing
+function createDataFolder() {
+	const dataFolder = './data';
+	if (!fs.existsSync(dataFolder)) {
+		fs.mkdir(dataFolder);
+		console.log('Created the `data` folder...');
+	} else {
+		console.log('The `data` folder already exists...');
+	}
+}
+createDataFolder();
+
+// Log to the console that the script was run
+console.log("✅ scraper.js was run!");
