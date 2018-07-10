72a3c64da6b0bfe2d353747467bae521ae74541f
Dan Robertson <dan@productintuition.com>
2018-07-09 21:06:37 -0700

    Refactor and clean up scraper code

--- scraper.js
+++ scraper.js
@@ -1,34 +1,43 @@
 // Require modules
 const fs = require('fs'); // For file system interactions
-const Crawler = require('crawler');
-const Json2csvParser = require('json2csv').Parser;
+const Crawler = require('crawler'); // For the Crawler npm module
+const Json2csvParser = require('json2csv').Parser; // For the json2csv npm module
+
+// Delcar vars to crawl homepage for product links
+const entryPage = 'http://shirts4mike.com/shirts.php';
+let productLinks = [];
+
+// Declare array of objects to hold scraped product data
+let scrapedData = [];
 
 // Create `data` folder, if not already existing
 function createDataFolder() {
 	const dataFolder = './data';
 	if (!fs.existsSync(dataFolder)) {
-		fs.mkdir(dataFolder);
-		console.log('Created the `data` folder...');
+		fs.mkdir(dataFolder, (err) => {
+			if(err) {
+				appendToErrorLog(err);
+				return console.log(err);
+			}
+		});
+		console.log('Writing scraped data to the `data` folder...');
 	} else {
-		console.log('The `data` folder already exists...');
+		console.log('Writing scraped data to a new file in the existing `data` folder...');
 	}
 }
 createDataFolder();
 
-// Crawl homepage for product links
-const homepage = 'http://shirts4mike.com/shirts.php';
-let productLinks = [];
-
 	// Helper functions
+	// ---------------
 // Make relative path into absolute URL
-function absoluteUrl(relativeUrl) {
+function getAbsoluteUrl(relativeUrl) {
 	return `http://shirts4mike.com/${relativeUrl}`;
 }
 
 // Print human-readable errors
-function printError(error) {
+function printConnectionError(error) {
 	appendToErrorLog(error);
-	console.error(`\n \n Sorry, cannot connect to ${homepage}. Please check your Internet connection and try again. \n (${error}) \n \n`);
+	console.error(`\n \n Sorry, cannot connect to ${entryPage}. Please check your Internet connection and try again. \n (${error}) \n \n`);
 }
 
 // Save CSV file
@@ -36,62 +45,65 @@ function saveCsvFile(csv) {
 	const date = (new Date()).toISOString().split('T')[0];
 	fs.writeFile(`./data/${date}.csv`, csv, function(err) {
 		if(err) {
-				return console.log(err);
+			appendToErrorLog(err);
+			return console.log(err);
 		}
-		console.log("\n ✅ \n The scraped product data was saved!\n");
-	}); 
+	});
+	if (productLinks.length === scrapedData.length) {
+		console.log("\n✅ The scraped product data was saved! ✅\n");
+	} 
 }
 
 // Append error to log file
 function appendToErrorLog(error) {
-	const logFile = 'scraper-error.log';
-	const timeStamp = new Date().toString();
-	const logLine = `[${timeStamp}] ${error} \n`;
-	fs.appendFileSync(logFile, logLine);
+	const logLine = `[${new Date().toString()}] ${error} \n`;
+	fs.appendFileSync('scraper-error.log', logLine);
 }
 
+// Write data to CSV
+function writeProductsCsv(products) {
+	// Convert scrapedData to csv
+	const fields = ['Title', 'Price', 'ImageURL', 'URL', 'Time'];
+	const productsParser = new Json2csvParser({fields});
+	const csv = productsParser.parse(products);
+	// Save file to disk
+	saveCsvFile(csv);
+}
 
-// Declare array of objects (to-be attrs: Title, Price, ImageURL, URL, Time)
-let scrapedData = [];
+	// Scraper callback functions
+	// ------------------------
 
-	// Callback for getting product links
+// Callback for getting product links
 function getProductLinks(error, res, done) {
 	if(error) {
-		printError(error);
+		printConnectionError(error);
 	} else {
 		let $ = res.$;
 		$('a').each(function(i, elem) {
 			let link = $(this).attr('href');
 			if (link.startsWith('shirt.php?')) {
-				// Make link the absolute address, not relative
-				link = link;
 				// Add link to productLinks array
-				productLinks.push(absoluteUrl(link));
+				productLinks.push(getAbsoluteUrl(link));
 			}
 		});
+		// Crawl all product pages, from productLinks array
 		crawlProductPage.queue(productLinks);
 	}
 	done();
-	// ⚠️ Fix this timeout hack (not sure how yet)
-	setTimeout(() => {
-		writeProductsCsv(scrapedData)
-	}, 2000);
 }
 
-	// Callback for getting product links
+// Callback for getting product links
 function getProductData(error, res, done) {
 	if(error) {
-		printError(error);
+		printConnectionError(error);
 	} else {
 		let $ = res.$;
 		// Select relavant page $elements (Title, Price, ImageURL, URL, Time)
-		const date = new Date();
-
 		const title = $('title').text();
 		const price = $('.price').text();
-		const imageUrl = absoluteUrl($('.shirt-picture > span > img').attr('src'));
+		const imageUrl = getAbsoluteUrl($('.shirt-picture > span > img').attr('src'));
 		const url = res.options.uri;
-		const time = `${date.getUTCHours()}:${date.getUTCMinutes()}:${date.getUTCSeconds()}`
+		const time = new Date().toUTCString();
 		// Construct object for given product
 		const product = {
 			Title : title,
@@ -102,28 +114,22 @@ function getProductData(error, res, done) {
 		};
 		// Add object to scrapedData array
 		scrapedData.push(product);
+		writeProductsCsv(scrapedData);
 	}
 	done();
 }
 
-	// Write data to CSV
-function writeProductsCsv(products) {
-	// Convert scrapedData to csv
-	const fields = ['Title', 'Price', 'ImageURL', 'URL', 'Time'];
-	const productsParser = new Json2csvParser({fields});
-	const csv = productsParser.parse(products);
-	// Save file to disk
-	saveCsvFile(csv);
-}
-
-const crawlHomepage = new Crawler({
+// Instantiating Crawler for homepage
+const crawlEntryPage = new Crawler({
 	maxConnections : 10,
 	callback : getProductLinks
 });
 
+// Instantiating Crawler for product pages
 const crawlProductPage = new Crawler({
 	maxConnections : 10,
 	callback : getProductData
 })
 
-crawlHomepage.queue(homepage);
+// Queuing entry page URL for crawling
+crawlEntryPage.queue(entryPage);
